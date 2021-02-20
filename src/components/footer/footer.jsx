59f48c808a8939c7d7c900c6485fd940c8e620f6
Evgenia Alfimova <alfimova.evg@gmail.com>
2021-02-20 16:43:43 +0200

    добавлен sass и шрифты

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -2,15 +2,17 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <ul>
-      <li><a href='#'>Корпоративным клиентам</a></li>
-      <li><a href='#'>Клиентам</a></li>
-      <li><a href='#'>Аренда авто</a></li>
-      <li><a href='#'>Каршеринг</a></li>
-      <li><a href='#'>Как продать авто</a></li>
-      <li><a href='#'>Traid-in</a></li>
-      <li><a href='#'>Test draiv</a></li>
-    </ul>
+    <footer className="container">
+      <ul>
+        <li><a href='#'>Корпоративным клиентам</a></li>
+        <li><a href='#'>Клиентам</a></li>
+        <li><a href='#'>Аренда авто</a></li>
+        <li><a href='#'>Каршеринг</a></li>
+        <li><a href='#'>Как продать авто</a></li>
+        <li><a href='#'>Traid-in</a></li>
+        <li><a href='#'>Test draiv</a></li>
+      </ul>
+    </footer>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
 import slides from './mocks/slides';
+import './css/style.scss';
 
 ReactDOM.render(
   <App images={slides}/>,
